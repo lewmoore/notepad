1195f4bfaa051f7a0dbecec37bfa5ff033ab2308
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-02-13 15:12:27 +0000

    Added extra assert for to contain

--- spec/assert.js
+++ spec/assert.js
@@ -1,22 +1,18 @@
 var assert =  {
-  isEqual: function(actual, expected) {
-  if(actual !== expected) {
-    //Run if failed
-    throw {type: "Fail", details: {actual: actual, expected: expected}};
-  } else {
-    //Run if successful
-    console.log("Success", {type: "Test passed", details: {actual: actual, expected: expected}});
-  }
-}
-}
 
+    isEqual: function(actual, expected) {
+    if(actual !== expected) {
+      throw {type: "Fail", details: {actual: actual, expected: expected}};
+    } else {
+      console.log("Success", {type: "Test passed", details: {actual: actual, expected: expected}});
+    }
+  },
 
-var assert = {
-isNotEqual: function(actual, expected) {
-  if(actual === expected) {
-    throw {type: "Fail", details: {actual: actual, expected: expected}};
-  } else {
-    console.log("Success", {type: "Test passed", details: {actual: actual, expected:expected}});
-  }
-}
-}
+    toContain: function (actual, expected) {
+      if (actual.includes(expected.toLowerCase())) {
+        console.log("Success", {type: "Test passed", details: {actual: actual, expected: expected}});
+      } else {
+        throw {type: "Fail", details: {actual: actual, expected: expected}};
+      }
+    }
+};
