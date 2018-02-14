dd60af486eac3eb692bc163cbb699be9d8fbf491
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-14 13:27:01 +0000

    first DOM manipulation example

--- models/note-model.js
+++ models/note-model.js
@@ -4,3 +4,12 @@
 // var pathToHtmlAndJsFiles = path.join(__dirname, "./index.html");
 // var server = httpServer.createServer({ root: pathToHtmlAndJsFiles });
 // server.listen(3000);
+
+
+// var element = document.getElementById("app").textContent = "The persian";
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  document.getElementById("app").textContent = "ben loves Atari";
+
+});
