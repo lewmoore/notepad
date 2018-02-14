008bddc53a581f7f20d2200f83d14050f662547b
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-02-14 17:16:06 +0000

    tested green for listing notes 20 characters

--- spec/notepad-test.js
+++ spec/notepad-test.js
@@ -5,9 +5,17 @@ var notepad = new Notepad();
   });
 
   it('Notepad stores notes in array', function(){
-    var note = new Note('I hate Jasmine')
-    var note2 = new Note('Jasmine')
-    notepad._addToArray(note)
-    assert.arrayContain(notepad.notes, 'I hate Jasmine')
+    var note = new Note('I hate Jasmine');
+    var note2 = new Note('Jasmine');
+    notepad._addToArray(note);
+    assert.arrayContain(notepad.notes, 'I hate Jasmine');
+
+    it('Should print out 20 characters', function () {
+      var note = new Note('I love to code and test drive all of my development');
+      notepad._addToArray(note);
+      var test = notepad.listNotes();
+      console.log(notepad.listNotes());
+      assert.isEqual(test , "I love to code and ");
+    });
   });
 });
