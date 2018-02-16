464ccc2261f05eb0da3f9228f9627473102836cb
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-16 13:51:26 +0000

    add some css

--- spec/feature/seeFullNoteSpec.js
+++ spec/feature/seeFullNoteSpec.js
@@ -3,12 +3,35 @@ describe("#See the full note", function () {
   it("Should display full note on clicking note list", function () {
     //window.location.reload();
 
-    document.getElementById("textarea").textContent = "This is the seoncd test!";
+    document.getElementById("textarea").textContent = "This is the second test!";
     document.getElementById("addNote").click();
 
+    console.log(1);
     console.log("after add note");
 
     document.getElementById("number-1").click();
+    var content = getElementById("number-1").value
+    console.log(2);
+    console.log(content);
+    console.log(3);
+    console.log(content.textContent);
+    assert.isEqual(content.textContent, "This is the second test!");
 
 });
 });
+
+//
+// describe("#Creating a note", function () {
+//
+//   it("Should capture text from text box on click", function () {
+//     document.getElementById("textarea").textContent = "This is a test!";
+//     document.getElementById("addNote").click();
+
+//     var content = document.querySelector('li');
+//
+//     assert.isEqual(content.textContent,"This is a test!...");
+//
+//   });
+// });
+//
+//
