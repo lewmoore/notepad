912b734b0e3afc0d7b22475aeef630b1696d09f5
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-02-16 11:16:34 +0000

    created a second feature test

--- spec/feature/createNoteSpec.js
+++ spec/feature/createNoteSpec.js
@@ -2,18 +2,25 @@
   describe("#Creating a note", function () {
 
     it("Should capture text from text box on click", function () {
-      console.log("textarea");
-
       document.getElementById("textarea").textContent = "This is a test!";
       document.getElementById("addNote").click();
-
-      console.log("after click");
       var content = document.querySelector('li');
-      console.log(content.textContent);
 
       assert.isEqual(content.textContent,"This is a test!...");
 
-
     });
   });
-  
+
+  describe("#See the full note", function () {
+
+    it("Should display full note on clicking note list", function () {
+      //window.location.reload();
+
+      document.getElementById("textarea").textContent = "This is the seoncd test!";
+      document.getElementById("addNote").click();
+
+      console.log("after add note");
+
+
+  });
+});
