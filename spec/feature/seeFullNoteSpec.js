c1ccf0b4fc86b9cafcb37acebdab07444893b9fd
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-02-16 17:08:58 +0000

    Update seeFullNoteSpec.js

--- spec/feature/seeFullNoteSpec.js
+++ spec/feature/seeFullNoteSpec.js
@@ -20,18 +20,3 @@ describe("#See the full note", function () {
 });
 });
 
-//
-// describe("#Creating a note", function () {
-//
-//   it("Should capture text from text box on click", function () {
-//     document.getElementById("textarea").textContent = "This is a test!";
-//     document.getElementById("addNote").click();
-
-//     var content = document.querySelector('li');
-//
-//     assert.isEqual(content.textContent,"This is a test!...");
-//
-//   });
-// });
-//
-//
