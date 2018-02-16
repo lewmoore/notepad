767fb133985d73ca03093fa4cd81f428cba8e1b9
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-02-16 17:09:20 +0000

    Update createNoteSpec.js

--- spec/feature/createNoteSpec.js
+++ spec/feature/createNoteSpec.js
@@ -1,14 +1,14 @@
-  // 
-  // describe("#Creating a note", function () {
-  //
-  //   it("Should capture text from text box on click", function () {
-  //     document.getElementById("textarea").textContent = "This is a test!";
-  //     document.getElementById("addNote").click();
-  //     var content = document.querySelector('li');
-  //
-  //     assert.isEqual(content.textContent,"This is a test!...");
-  //
-  //   });
-  // });
-  //
-  //
+  
+  describe("#Creating a note", function () {
+  
+    it("Should capture text from text box on click", function () {
+      document.getElementById("textarea").textContent = "This is a test!";
+      document.getElementById("addNote").click();
+      var content = document.querySelector('li');
+  
+      assert.isEqual(content.textContent,"This is a test!...");
+  
+    });
+  });
+  
+  
